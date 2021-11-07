d53453bc11144672c3349460e053a16297d6acce
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-07 17:02:44 +0530

    printing prime factors of n by itteraring till i*i for efficiency

--- src/com/coreprograms/PrimeFactors.java
+++ src/com/coreprograms/PrimeFactors.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 
 public class PrimeFactors {
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.print("enter n value = ");
-		
-		
+		int n = scan.nextInt();
+
+		System.out.print("prime factors of " + n + " : ");
+
+		for (int i = 2; i * i <= n; i++) {
+			while (n % i == 0) {
+				System.out.print(i + " ");
+				n = n / i;
+			}
+
+		}
+		if(n>1) {
+			System.out.println(n);
+		}
 	}
 
 }
